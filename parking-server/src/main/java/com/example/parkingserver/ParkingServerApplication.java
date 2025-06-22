a4dfb5af73f5a3b8b61537cc7aac27c3889f9631
Thushini Akashi <thushiniakashi58@gmail.com>
2025-06-22 21:26:19 +0530

    parking-server create

---
+++ parking-server/src/main/java/com/example/parkingserver/ParkingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingServerApplication.class, args);
+	}
+
+}
