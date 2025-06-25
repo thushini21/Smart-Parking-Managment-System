11d3cbdf9f2803253d86a7c43519d1a252187dbc
Thushini Akashi <thushiniakashi58@gmail.com>
2025-06-25 13:06:45 +0530

    create databse.yml file and config in config server

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -2,8 +2,16 @@ package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
