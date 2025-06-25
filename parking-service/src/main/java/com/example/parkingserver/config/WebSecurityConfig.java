123cd496d0ba498adcb8deecc80b637dbeabb658
Thushini Akashi <thushiniakashi58@gmail.com>
2025-06-25 21:11:36 +0530

    some update in parking service

--- parking-service/src/main/java/com/example/parkingserver/config/WebSecurityConfig.java
+++ parking-service/src/main/java/com/example/parkingserver/config/WebSecurityConfig.java
@@ -1,23 +1,22 @@
-package com.example.parkingserver.config;
+package com.example.paymentserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow all requests without auth
+                );
         return http.build();
     }
 
@@ -26,5 +25,3 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
-
